c3de4e74b38e80144988f67686256168ef5804f3
Unknown <jjdjordjevic@gmail.com>
2018-05-26 13:49:09 +0200

    Add tokens.py

---
+++ tokens.py
@@ -0,0 +1,21 @@
+import lexer
+
+RESERVED = 'RESERVED'
+INT = 'INT'
+ID = 'ID'
+OPERATOR = 'OPERATOR'
+PRINT = 'PRINT'
+EQUALS = 'EQUALS'
+
+token_exprs = [
+    (r'[\n\t]+',               None)
+    (r'#[^\n]*',                None)
+    (r'\=',                     EQUALS)
+    (r'\+',                     OPERATOR)
+    (r'-',                      OPERATOR)
+    (r'\*',                     OPERATOR)
+    (r'/',                      OPERATOR)
+    (r'print',                  PRINT)
+    (r'[0-9]+',                 INT)
+    (r'[A-Za-z][A-Za-z0-9_]',   ID)
+]
