0b416cb61f92312c915d5b5c4821e67fdb9c9b3c
Unknown <jjdjordjevic@gmail.com>
2018-09-30 16:25:01 +0200

    Završen deo parsera sa kombinatorima

---
+++ kombinatori_parser.py
@@ -0,0 +1,149 @@
+class Result:                                               #Klasa/tip koju će svaka metoda od sledećih da vrati (Vrednost - taj token, i Pozcija - sledeći token)
+    def __init__(self, value, pos):
+        self.value = value
+        self.pos = pos
+
+    def __repr__(self):
+        return 'Result(%s, %d)' % (self.value, self.pos)
+
+class Parser:
+    def __call__(self, tokens, pos):
+        return None 
+
+    def __add__(self, other):
+        return Concat(self, other)
+
+    def __mul__(self, other):
+        return Exp(self, other)
+
+    def __or__(self, other):
+        return Alternate(self, other)
+
+    def __xor__(self, function):
+        return Process(self, function)
+
+class Reserved(Parser):                                     #Rezervisane reči, operatori
+    def __init__(self, value, tag):
+        self.value = value
+        self.tag = tag
+
+    def __call__(self, tokens, pos):
+        if pos < len(tokens) and \
+           tokens[pos][0] == self.value and \
+           tokens[pos][1] is self.tag:
+            return Result(tokens[pos][0], pos + 1)
+        else:
+            return None
+
+class Tag(Parser):                                          #Jedinstveni tagovi (poput DODELA, ISPIS, KRAJ, BROJ, IME...)
+    def __init__(self, tag):
+        self.tag = tag
+
+    def __call__(self, tokens, pos):
+        if pos < len(tokens) and tokens[pos][1] is self.tag:
+            return Result(tokens[pos][0], pos + 1)
+        else:
+            return None
+
+class Concat(Parser):                                       #Kombinator koji spaja dva susedna parsovana tokena i vraća funkciju Result 
+    def __init__(self, left, right):                        #sa sledećim parametrima: spojeni pars i sledeći desni token #__ADD__
+        self.left = left
+        self.right = right
+
+    def __call__(self, tokens, pos):
+        left_result = self.left(tokens, pos)
+        if left_result:
+            right_result = self.right(tokens, left_result.pos)
+            if right_result:
+                combined_value = (left_result.value, right_result.value)
+                return Result(combined_value, right_result.pos)
+        return None
+
+class Alternate(Parser):                                    #Kombinator koji se ponaša kao OR, pa tako vraća None isključivo kada su 
+    def __init__(self, left, right):                        #oba parsera neuspela #__OR__
+        self.left = left
+        self.right = right
+
+    def __call__(self, tokens, pos):
+        left_result = self.left(tokens, pos)
+        if left_result:
+            return left_result
+        else:
+            right_result = self.right(tokens, pos)
+            return right_result
+
+class Opt(Parser):                                          #Kombinator koji parsira u glavnom tekst poput if i else stejtmenta
+    def __init__(self, parser):                             
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result:
+            return result
+        else:
+            return Result(None, pos)        
+
+class Rep(Parser):                                          #
+    def __init__(self, parser):
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        results = []
+        result = self.parser(tokens, pos)
+        while result:
+            results.append(result.value)
+            pos = result.pos
+            result = self.parser(tokens, pos)
+        return Result(results, pos)
+
+class Process(Parser):                                      #
+    def __init__(self, parser, function):
+        self.parser = parser
+        self.function = function
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result:
+            result.value = self.function(result.value)
+            return result
+
+class Lazy(Parser):
+    def __init__(self, parser_func):
+        self.parser = None
+        self.parser_func = parser_func
+
+    def __call__(self, tokens, pos):
+        if not self.parser:
+            self.parser = self.parser_func()
+        return self.parser(tokens, pos)
+
+class Phrase(Parser):                                       #Glavni - završni kombinator koji proverava da li su svi tokeni parsovani, da li je kod gotov
+    def __init__(self, parser):
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result and result.pos == len(tokens):
+            return result
+        else:
+            return None
+
+class Exp(Parser):                                          #Kombinator zaslužan za parsiranje jedinjenja razdvojenih jednim separatorom (to može biti bilo koji
+    def __init__(self, parser, separator):                  #operator ili tačka-zarez)
+        self.parser = parser
+        self.separator = separator
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+
+        def process_next(parsed):
+            (sepfunc, right) = parsed
+            return sepfunc(result.value, right)
+        next_parser = self.separator + self.parser ^ process_next
+
+        next_result = result
+        while next_result:
+            next_result = next_parser(tokens, result.pos)
+            if next_result:
+                result = next_result
+        return result               
